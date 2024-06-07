30062ade39a5f4b30adbf8cfbb86c18042ddf4f9
marlenejodom <jodommatsoko@et.esiea.fr>
2024-06-07 18:13:59 +0200

    play music et register

--- SpotyMike/src/app/app.routes.ts
+++ SpotyMike/src/app/app.routes.ts
@@ -1,27 +1,22 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
+
   {
     path: 'login',
     loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
   },
   {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
     path: 'register',
     loadComponent: () => import('./register/register.page').then( m => m.RegisterPage)
   },
+
   {
-    path: 'list',
-    loadComponent: () => import('./list/list.page').then( m => m.ListPage)
-  },  {
     path: 'play',
     loadComponent: () => import('./play/play.page').then( m => m.PlayPage)
   },
